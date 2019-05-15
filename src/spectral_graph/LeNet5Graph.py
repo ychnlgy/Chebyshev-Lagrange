659ccb000aed289b92b14ffc60ec0c9d1ff00588
Yuchen Li <ychnlgy@gmail.com>
2019-05-14 22:44:38 -0400

    Trying RegAct

--- src/spectral_graph/LeNet5Graph.py
+++ src/spectral_graph/LeNet5Graph.py
@@ -161,7 +161,7 @@ class LeNet5Graph(torch.nn.Module):
         self.net = torch.nn.Sequential(
             LeNet5.create_fc(fc1fin, fc1),
             relu,
-            torch.nn.Dropout(0.5),
+            torch.nn.Dropout(0.1),
             LeNet5.create_fc(fc1, fc2)
         )
 
