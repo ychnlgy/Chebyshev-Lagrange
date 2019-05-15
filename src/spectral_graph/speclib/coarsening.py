762cfdf8fd6782fa5c7f97e3fb3bb86998410c38
Yuchen Li <ychnlgy@gmail.com>
2019-05-15 18:08:43 -0400

    Debugging spectral cnn

--- src/spectral_graph/speclib/coarsening.py
+++ src/spectral_graph/speclib/coarsening.py
@@ -57,7 +57,7 @@ def coarsen(A, levels):
         Mnew, Mnew = A.shape
         print('Layer {0}: M_{0} = |V| = {1} nodes ({2} added), |E| = {3} edges'.format(i, Mnew, Mnew-M, A.nnz//2))
 
-        L = laplacian(A, normalized=True)
+        L = laplacian(A, normalized=False)
         laplacians.append(L)
         
     return laplacians, perms[0] if len(perms) > 0 else None
